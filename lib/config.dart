3b4cf194f1b6b9707002aa797f38caa05db38395
arjunan.k.s.a.t <arjunan.k.s.a.t@gmail.com>
2022-02-10 15:14:40 +0530

    Project Added

---
+++ lib/config.dart
@@ -0,0 +1,10 @@
+
+class Config {
+  // static const String APP_KEY = 'YunXin NERTC Key';
+  /// YunXin NERTC Key
+  static const APP_KEY = '';
+
+  /// authpack
+  static const List<int> auth = [];
+}
+
